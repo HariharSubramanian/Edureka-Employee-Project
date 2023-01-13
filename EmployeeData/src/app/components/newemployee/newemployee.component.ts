e751786b5e85e9a7d816f209ef8e231a85d26a81
Harihar <hari.harihar20@gmail.com>
2023-01-13 16:56:40 +0530

    new empdata and services final update

--- EmployeeData/src/app/components/newemployee/newemployee.component.ts
+++ EmployeeData/src/app/components/newemployee/newemployee.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewEmpDataService } from 'src/app/services/new-emp-data.service';
 import { NewEmployeeData } from './employee2';
 
 @Component({
@@ -6,6 +7,8 @@ import { NewEmployeeData } from './employee2';
   templateUrl: './newemployee.component.html',
   styleUrls: ['./newemployee.component.css']
 })
-export class NewemployeeComponent {
+export class NewemployeeComponent implements OnInit {
   public tableName="Employees data from API";
+  constructor(private newempdata:NewEmpDataService){}
+  ngOnInit():void{}
 }
